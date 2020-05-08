8af137ecce6b36de255b04d0a53d89bfed3e4aa9
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-05-08 12:54:19 -0500

    last commit before lunch

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,39 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    
+})
+
+
+
+
+function createCard(attrs) {
+    // define new elements
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const img = document.createElement('img')
+    const authorName = document.createElement('span')
+    //set up structure
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    author.appendChild(authorName)
+    imgContainer.appendChild(img)
+    // add class names
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+    // add content
+    headline.textContent = attrs.headline
+    img.src = attrs.authorPhoto
+    authorName.textContent = attrs.authorName
+    // return
+    return card
+}
